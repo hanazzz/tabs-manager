9a2046f3569dda205bbcbf5ba321cbebfeae57b4
Hana Zait <hana.o.zait@gmail.com>
2023-02-10 00:27:33 -0800

    Add ability to get list of tabs and focus on a tab

---
+++ popup.js
@@ -0,0 +1,38 @@
+// Retrieve tabs from matching URLs
+const tabs = await chrome.tabs.query({
+  url: [
+    "https://developer.chrome.com/docs/webstore/*",
+    "https://developer.chrome.com/docs/extensions/*",
+  ],
+});
+
+// Sort tabs array alphabetically by tab name
+const collator = new Intl.Collator();
+tabs.sort((a, b) => collator.compare(a.title, b.title));
+
+// Get template to use for each tab
+const template = document.getElementById("li_template");
+const elements = new Set();
+for (const tab of tabs) {
+  // Clone template and populate with tab title and pathname
+  const element = template.content.firstElementChild.cloneNode(true);
+
+  const title = tab.title.split("-")[0].trim();
+  const pathname = new URL(tab.url).pathname.slice("/docs".length);
+
+  element.querySelector(".title").textContent = title;
+  element.querySelector(".pathname").textContent = pathname;
+
+  // When user clicks tab list item, focus on that tab and window
+  element.querySelector("a").addEventListener("click", async () => {
+    // need to focus window as well as the active tab
+    await chrome.tabs.update(tab.id, { active: true });
+    await chrome.windows.update(tab.windowId, { focused: true });
+  });
+
+  // Add new element to elements Set
+  elements.add(element);
+}
+
+// Add tabs to list
+document.querySelector("ul").append(...elements);
